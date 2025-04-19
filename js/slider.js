8c8cc47751bd1ba41d80b83ad5d4d8dd8a5e3186
lena050177 <lena.ydina@gmail.com>
2025-04-19 04:52:33 +0200

    slider

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#slides').slidesjs({
+      width: 910,
+      height: 463,
+      play: {
+        active: true,
+        auto: true,
+        interval: 3000,
+        swap: true
+      }
+    });
+  });
